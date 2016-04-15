638769bd63288c43d36a86964d9612607c9c04b9
Andrew Carnes <carnesandnoble@gmail.com>
2016-04-15 14:20:26 -0400

    Cross checked with v1 and updated the code to reproduce the results. Added dR comparison to JetSelectionTools since we moved the dR checking away from CMSSW for now.

--- bin/synch.cxx
+++ bin/synch.cxx
@@ -5,6 +5,7 @@
 #include "SelectionCuts.h"
 #include "CategorySelection.h"
 #include "JetSelectionTools.h"
+#include "EventTools.h"
 
 #include "TLorentzVector.h"
 
@@ -13,28 +14,6 @@
 #include <vector>
 #include <utility>
 
-//////////////////////////////////////////////////////////////////
-//---------------------------------------------------------------
-//////////////////////////////////////////////////////////////////
-
-bool sameRunAndEvent(std::pair<int,int> a, std::pair<int,int> b)
-{
-   if(a.first == b.first && a.second == b.second) return true;
-   else return false;
-}
-
-//////////////////////////////////////////////////////////////////
-//---------------------------------------------------------------
-//////////////////////////////////////////////////////////////////
-
-bool eventInVector(std::pair<int,int> e, std::vector<std::pair<int,int>> events)
-{
-    for(unsigned int i=0; i<events.size(); i++)
-    {
-        if(sameRunAndEvent(e, events[i])) return true;
-    }
-    return false;
-}
 
 //////////////////////////////////////////////////////////////////
 //---------------------------------------------------------------
@@ -57,7 +36,8 @@ int main(int argc, char* argv[])
     // SAMPLES---------------------------------------------------------
     ///////////////////////////////////////////////////////////////////
  
-    TString datafilename = TString("/cms/data/store/user/t2/users/acarnes/h2mumu/samples/stage1/data_from_json/25ns/golden/stage_1_singleMuon_RunCD_GOLDEN_ALL.root");
+    //TString datafilename = TString("/cms/data/store/user/t2/users/acarnes/h2mumu/samples/stage1/data_from_json/25ns/golden/stage_1_singleMuon_RunCD_GOLDEN_ALL.root");
+    TString datafilename = TString("/cms/data/store/user/t2/users/acarnes/h2mumu/samples/stage1/data_from_json/25ns/golden/stage_1_singleMuon_RunDBoth_MINIAOD_GOLDEN_ALL.root");
     Sample* datasample = new Sample(datafilename, "Data", "data");
     datasample->lumi = 2260;
     datasample->pileupfile = "/cms/data/store/user/t2/users/acarnes/h2mumu/samples/stage1/data_from_json/25ns/golden/pileup/PUCalib_Golden_71mb.root";
@@ -100,7 +80,7 @@ int main(int argc, char* argv[])
     }
 
     ///////////////////////////////////////////////////////////////////
-    // Do Stuff -------------------------------------------------------
+    // Cut and Categorize ---------------------------------------------
     ///////////////////////////////////////////////////////////////////
     
     // Objects to help with the cuts and selections
@@ -110,13 +90,21 @@ int main(int argc, char* argv[])
     SynchEventSelectionCuts synchEventSelection;
     SynchMuonSelectionCuts synchMuonSelection;
 
-    // Count the number of events in each category
-    int loose01 = 0;
-    int tight01 = 0;
-    int VBFTight = 0;
-    int GGFTight = 0;
-    int VBFLoose = 0;
-    int n2jets = 0;
+    // Keep track of the events are in each category
+    std::vector<std::pair<int,long long int>> VBFTightVec;
+    std::vector<std::pair<int,long long int>> GGFTightVec;
+    std::vector<std::pair<int,long long int>> VBFLooseVec;
+    std::vector<std::pair<int,long long int>> loose01Vec;
+    std::vector<std::pair<int,long long int>> tight01Vec;
+
+    // Keep track of the events from the cross-check list
+    std::vector<std::pair<int, long long int>> xmissingVec;
+    std::vector<std::pair<int,long long int>> xVBFTightVec;
+    std::vector<std::pair<int,long long int>> xGGFTightVec;
+    std::vector<std::pair<int,long long int>> xVBFLooseVec;
+    std::vector<std::pair<int,long long int>> xloose01Vec;
+    std::vector<std::pair<int,long long int>> xtight01Vec;
+
 
     // Output information of the first numToPrint events
     int numToPrint = 0;
@@ -124,16 +112,8 @@ int main(int argc, char* argv[])
 
     // std::pair(run, event)
     // The list of events Adrian gave us to look at
-    std::vector<std::pair<int,int>> eventsToCheck;
-
-    // v1 extra
-    eventsToCheck.push_back( std::pair<int,int>(257751, 159317578));
-    eventsToCheck.push_back( std::pair<int,int>(257751, 159510027));
-    eventsToCheck.push_back( std::pair<int,int>(257751, 11422285 ));
-    eventsToCheck.push_back( std::pair<int,int>(257751, 18707162 ));
-    eventsToCheck.push_back( std::pair<int,int>(257751, 19684441 ));
-    eventsToCheck.push_back( std::pair<int,int>(257751, 21454581 ));
-
+    std::vector<std::pair<int,long long int>> eventsToCheck;
+    loadEventsFromFile("txt/andrew/xMissing.txt", eventsToCheck);
 
     // Loop through the events in the data sample
     Sample* s = samples["Data"];
@@ -142,115 +122,61 @@ int main(int argc, char* argv[])
     {
        s->getEntry(i); 
        s->vars.validJets = std::vector<TLorentzVector>();
-       jetSelectionTools.getValidJets(s->vars.jets, s->vars.validJets);
+       jetSelectionTools.getValidJetsdR(s->vars, s->vars.validJets);
        std::pair<int,int> e(s->vars.eventInfo.run, s->vars.eventInfo.event); // create a pair that identifies the event uniquely
 
-       if(!tightMuonId.evaluate(s->vars)) continue;
-       if(!synchEventSelection.evaluate(s->vars)) continue;
-       if(!synchMuonSelection.evaluate(s->vars)) continue;
-
-       //std::cout << "========= RUN: " << e.first << ", EVENT: " << e.second << " =============" << std::endl;
-
-       categorySelection.evaluate(s->vars);
-       if(categorySelection.isVBFTight) VBFTight++;
-       else if(categorySelection.isGGFTight) GGFTight++;
-       else if(categorySelection.isVBFLoose) VBFLoose++;
-       else if (categorySelection.isTight01) tight01++;
-       else if (categorySelection.isLoose01) loose01++;
-
-       if(eventInVector(e, eventsToCheck) || i < numToPrint) // Adrian gave a list of events to look at for synch purposes
-       //if(false)
-       {
-         numCounted++;
-         std::cout << "========= RUN: " << e.first << ", EVENT: " << e.second << " =============" << std::endl;
-         std::cout << "  Count #: " << i << std::endl;
-         std::cout << std::endl;
-         std::cout << "  recoCandMass: " << s->vars.recoCandMass << std::endl;
-         std::cout << "  recoCandPt: " << s->vars.recoCandPt << std::endl;
-         std::cout << std::endl;
-         std::cout << "  reco1.pt: " << s->vars.reco1.pt << std::endl;
-         std::cout << "  reco1.phi: " << s->vars.reco1.phi << std::endl;
-         std::cout << "  reco1.eta: " << s->vars.reco1.eta << std::endl;
-         std::cout << std::endl;
-         std::cout << "  reco2.pt: " << s->vars.reco2.pt << std::endl;
-         std::cout << "  reco2.phi: " << s->vars.reco2.phi << std::endl;
-         std::cout << "  reco2.eta: " << s->vars.reco2.eta << std::endl;
-         std::cout << std::endl;
-         std::cout << "  nJets: " << s->vars.jets.nJets << std::endl;
-         std::cout << "  nValidJets: " << s->vars.validJets.size() << std::endl;
-         std::cout << "  MET: " << s->vars.met.pt << std::endl;
-         std::cout << std::endl;
-
-         for(unsigned int j=0; j<s->vars.jets.nJets && j<10; j++)
-         {
-             std::cout << "  jet" << j << " pt: " <<  s->vars.jets.pt[j] << std::endl;
-             std::cout << "  jet" << j << " phi: " << s->vars.jets.phi[j] << std::endl;
-             std::cout << "  jet" << j << " eta: " << s->vars.jets.eta[j] << std::endl;
-             std::cout << std::endl;
-
-         }
-
-         std::cout << std::endl;
-
-         for(unsigned int j=0; j<s->vars.validJets.size(); j++)
-         {
-             std::cout << "  validjet" << j << " pt: " <<  s->vars.validJets[j].Pt() << std::endl;
-             std::cout << "  validjet" << j << " phi: " << s->vars.validJets[j].Phi() << std::endl;
-             std::cout << "  validjet" << j << " eta: " << s->vars.validJets[j].Eta() << std::endl;
-             std::cout << std::endl;
-
-         }
-         std::cout << std::endl;
-
-         if(categorySelection.isPreselected) 
-         {
-             TLorentzVector leadJet = s->vars.validJets[0];
-             TLorentzVector subLeadJet = s->vars.validJets[1];
-
-             double dEta = leadJet.Eta() - subLeadJet.Eta();
-             TLorentzVector dijet = leadJet + subLeadJet;
-             double dijetMass = dijet.M();
-
-             std::cout << "  $$pass2jet, " << "njets: " << s->vars.validJets.size() << " == 2, leadPt: " << s->vars.validJets[0].Pt() << " > 40, subleadPt: "
-             << s->vars.validJets[1].Pt() << "> 30, met: " <<  s->vars.met.pt << "< 40" << std::endl;
-
-             // ////////////////////////////////////////////////////////////////////////////
-             // ========= VBF TIGHT========================================================
-             // ////////////////////////////////////////////////////////////////////////////
-
-             if(categorySelection.isVBFTight)
-             {
-                 std::cout << "    CATEGORY: VBFTight, abs(dEta): " << TMath::Abs(dEta) << " > 3.5, m(jj): " << dijetMass<< " > 650" << std::endl;
-             }
-
-             // ////////////////////////////////////////////////////////////////////////////
-             // ========= GGF TIGHT========================================================
-             // ////////////////////////////////////////////////////////////////////////////
-
-             if(categorySelection.isGGFTight)
-             {
-                 std::cout << "    CATEGORY: GGFTight, m(jj): " << dijetMass << "> 250, pt(uu): " << s->vars.recoCandPt << " > 50" << std::endl;
-             }
-             
-             // ////////////////////////////////////////////////////////////////////////////
-             // ========= VBF LOOSE========================================================
-             // ////////////////////////////////////////////////////////////////////////////
-
-             if(categorySelection.isVBFLoose)
-             {
-                 std::cout << "    CATEGORY: VBFLoose, m(jj): " << dijetMass << " < 250, pt(uu): " << s->vars.recoCandPt << "< 50, abs(dEta): " << TMath::Abs(dEta) << ", < 3.5"<< std::endl;
-             }
-         }
-
-         else
-         {
-             std::cout << "  !!fail2jet, " << "njets: " << s->vars.validJets.size() << " == 2, leadPt: " << ((s->vars.validJets.size()>0)?s->vars.validJets[0].Pt():-999) << " > 40, subleadPt: "
-             <<  ((s->vars.validJets.size()>1)?s->vars.validJets[1].Pt():-999) << "> 30, met: " <<  s->vars.met.pt << "< 40" << std::endl;
-            
-             if(categorySelection.isTight01) std::cout << "    CATEGORY: 10tight, recoCandPt: " << s->vars.recoCandPt << std::endl;
-             if(categorySelection.isLoose01) std::cout << "    CATEGORY: 10loose, recoCandPt: " << s->vars.recoCandPt << std::endl;
-         }
+       
+       // The event is cut
+       if(!tightMuonId.evaluate(s->vars))
+       { 
+           if(eventInVector(e, eventsToCheck)) xmissingVec.push_back(e); 
+           continue; 
        }
+       if(!synchEventSelection.evaluate(s->vars))
+       { 
+           if(eventInVector(e, eventsToCheck)) xmissingVec.push_back(e);
+           continue; 
+       }
+       if(!synchMuonSelection.evaluate(s->vars)) 
+       {
+           if(eventInVector(e, eventsToCheck)) xmissingVec.push_back(e);  
+           continue; 
+       }
+   
+       // The event passes the cuts, see which category it falls into
+       categorySelection.evaluate(s->vars);
+       if(categorySelection.isVBFTight) 
+       {
+           VBFTightVec.push_back(e);
+           if(eventInVector(e, eventsToCheck)) xVBFTightVec.push_back(e);
+       }
+       else if(categorySelection.isGGFTight)
+       {
+           GGFTightVec.push_back(e);
+           if(eventInVector(e, eventsToCheck)) xGGFTightVec.push_back(e);
+       }
+       else if(categorySelection.isVBFLoose)
+       {
+           VBFLooseVec.push_back(e);
+           if(eventInVector(e, eventsToCheck)) xVBFLooseVec.push_back(e);
+       }
+       else if (categorySelection.isTight01)
+       {
+           tight01Vec.push_back(e);
+           if(eventInVector(e, eventsToCheck)) xtight01Vec.push_back(e);
+       }
+       else if (categorySelection.isLoose01)
+       {
+           loose01Vec.push_back(e);
+           if(eventInVector(e, eventsToCheck)) xloose01Vec.push_back(e);
+       }
+
+       //if(eventInVector(e, eventsToCheck) || i < numToPrint) // Adrian gave a list of events to look at for synch purposes
+       if(false)
+         // ouput pt, mass info etc
+         outputEvent(e, s->vars, categorySelection);
+
+       // Reset the flags in preparation for the next event
        categorySelection.reset();
     }
 
@@ -260,16 +186,40 @@ int main(int argc, char* argv[])
     
     std::cout << std::endl;
     std::cout << "=========== Category Counts ============" << std::endl;
-    std::cout << "VBFTight: " << VBFTight << std::endl;
-    std::cout << "GGFTight: " << GGFTight << std::endl;
-    std::cout << "VBFLoose: " << VBFLoose << std::endl;
-    std::cout << "10Tight: " << tight01 << std::endl;
-    std::cout << "10Loose: " << loose01 << std::endl;
+    std::cout << "VBFTight: " << VBFTightVec.size() << std::endl;
+    std::cout << "GGFTight: " << GGFTightVec.size() << std::endl;
+    std::cout << "VBFLoose: " << VBFLooseVec.size() << std::endl;
+    std::cout << "10Tight: " << tight01Vec.size() <<  std::endl;
+    std::cout << "10Loose: " << loose01Vec.size() <<  std::endl;
+    std::cout << std::endl;
 
-    int dimutotal = VBFTight + GGFTight + VBFLoose + tight01 + loose01;
+    int dimutotal = VBFTightVec.size() + GGFTightVec.size() + VBFLooseVec.size() + tight01Vec.size() + loose01Vec.size();
     std::cout << "total: " << dimutotal << std::endl;
-    std::cout << "xcheck: " << numCounted << std::endl;
+    std::cout << std::endl;
 
+    std::cout << "=========== xCheck Category Counts ============" << std::endl;
+    std::cout << "VBFTight: " << xVBFTightVec.size() << std::endl;
+    std::cout << "GGFTight: " << xGGFTightVec.size() << std::endl;
+    std::cout << "VBFLoose: " << xVBFLooseVec.size() << std::endl;
+    std::cout << "10Tight:  " << xtight01Vec.size() <<  std::endl;
+    std::cout << "10Loose:  " << xloose01Vec.size() <<  std::endl;
+    std::cout << "Cut:      " << xmissingVec.size() <<  std::endl;
+
+    int xdimutotal = xVBFTightVec.size() + xGGFTightVec.size() + xVBFLooseVec.size() + xtight01Vec.size() + xloose01Vec.size() + xmissingVec.size();
+    std::cout << std::endl;
+    std::cout << "total: " << xdimutotal << std::endl;
+    std::cout << "to check total: " << eventsToCheck.size() << std::endl;
+
+    std::cout << std::endl;
+    outputEventsToFile(xVBFTightVec, "txt/andrew/xVBFTight.txt");
+    outputEventsToFile(xGGFTightVec, "txt/andrew/xGGFTight.txt");
+    outputEventsToFile(xVBFLooseVec, "txt/andrew/xVBFLoose.txt");
+    outputEventsToFile(xtight01Vec, "txt/andrew/x10Tight.txt");
+    outputEventsToFile(xloose01Vec, "txt/andrew/x10Loose.txt");
+    outputEventsToFile(xmissingVec, "txt/andrew/xMissing.txt");
+
+
+    // Plot some things if we wish
     DiMuPlottingSystem* dps = new DiMuPlottingSystem();
 
     return 0;
--- selection/JetSelectionTools.cxx
+++ selection/JetSelectionTools.cxx
@@ -24,16 +24,18 @@ JetSelectionTools::JetSelectionTools()
 {
     cJetSelectionPtMin = 30;
     cJetSelectionEtaMax = 4.7;
+    cJetSelectiondRMax = 0.3;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //-----------------------------------------------------------------------------
 ///////////////////////////////////////////////////////////////////////////////
 
-JetSelectionTools::JetSelectionTools(float jetSelectionPtMin, float jetSelectionEtaMax)
+JetSelectionTools::JetSelectionTools(float jetSelectionPtMin, float jetSelectionEtaMax, float jetSelectiondRMax)
 {
     cJetSelectionPtMin = jetSelectionPtMin;
     cJetSelectionEtaMax = jetSelectionEtaMax;
+    cJetSelectiondRMax = jetSelectiondRMax;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -52,6 +54,19 @@ int JetSelectionTools::getNValidJets(_PFJetInfo& jets)
     }   
     return nValidJets;
 }
+///////////////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////
+
+float JetSelectionTools::dR(float eta1, float phi1, float eta2, float phi2) 
+{
+// Determine the number of valid jets using the given cuts
+
+    float dEta = eta2 - eta1; 
+    float dPhi = phi2 - phi1;
+    float dr = TMath::Sqrt(dEta*dEta + dPhi*dPhi); 
+    return dr; 
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //-----------------------------------------------------------------------------
@@ -60,6 +75,7 @@ int JetSelectionTools::getNValidJets(_PFJetInfo& jets)
 void JetSelectionTools::getValidJets(_PFJetInfo& jets, std::vector<TLorentzVector>& jetvec) 
 {
 // Determine the number of valid jets using the given cuts
+// Use this when the jets have already been cut by dR in CMSSW
 
     for(unsigned int j=0; j < jets.nJets && j < 10; ++j)
     {   
@@ -71,3 +87,30 @@ void JetSelectionTools::getValidJets(_PFJetInfo& jets, std::vector<TLorentzVecto
         }
     }   
 }
+
+///////////////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////
+
+void JetSelectionTools::getValidJetsdR(VarSet& vars, std::vector<TLorentzVector>& jetvec)
+{
+// Determine the number of valid jets using the given cuts
+// Cut jets by dR here instead of in CMSSW
+
+    for(unsigned int j=0; j < vars.jets.nJets && j < 10; ++j)
+    {
+        // Pt and Eta selections
+        if(vars.jets.pt[j] > cJetSelectionPtMin && TMath::Abs(vars.jets.eta[j]) < cJetSelectionEtaMax)
+        {
+            // dR vs muons selection
+            if(!(dR(vars.jets.eta[j], vars.jets.phi[j], vars.reco1.eta, vars.reco1.phi) < cJetSelectiondRMax) && !(dR(vars.jets.eta[j], vars.jets.phi[j], vars.reco2.eta, vars.reco2.phi) < cJetSelectiondRMax))
+            {
+                TLorentzVector jet4vec; 
+                jet4vec.SetPtEtaPhiM(vars.jets.pt[j],vars.jets.eta[j],vars.jets.phi[j],vars.jets.mass[j]);
+                // passes all selections, add to valid jets
+                jetvec.push_back(jet4vec);
+            }
+        }
+    }
+}
+
