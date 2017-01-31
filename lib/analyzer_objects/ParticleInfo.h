deff9667ff8f5a4f91b4608b86897ca26dfc5fb9
Andrew Carnes <carnesandnoble@gmail.com>
2017-01-31 11:36:07 -0500

    Forgot ParticleInfo.h

---
+++ lib/analyzer_objects/ParticleInfo.h
@@ -0,0 +1,19 @@
+
+#ifndef PARTICLE_INFO
+#define PARTICLE_INFO
+
+#include <vector>
+#include "TMath.h"
+#include "TString.h"
+#include "TLorentzVector.h"
+
+struct ParticleInfo {
+
+  virtual void init() = 0;
+  virtual TLorentzVector get4vec() = 0;
+  virtual TString outputInfo() = 0;
+  virtual Double_t iso() = 0;
+
+};
+
+#endif                                                                                                               
